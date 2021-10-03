62810613469cfce9dadf16a3a213aa9788d7feef
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-03 23:36:18 +0300

    Add index script

---
+++ src/scripts/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/index.css';
+
+import connectBD from './components/connectBD';
+import App from './components/App.jsx';
+
+connectBD(renderTable);
+function renderTable(data) {
+  // Render react dom after receiving data.
+  ReactDOM.render(
+    <React.StrictMode>
+      <App content={data} />
+    </React.StrictMode>,
+    document.getElementById('container'),
+  );
+}
